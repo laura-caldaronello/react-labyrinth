1fa17796f0b04c999759c7f498747f2b660315dd
laura.caldaronello <caldaronello.l@gmail.com>
2021-07-27 16:26:23 +0200

    quasi

--- src/index.js
+++ src/index.js
@@ -24,6 +24,133 @@ function whereInArrayOfArrays(bigArray,smallArray){
   return -1;
 }
 
+//condizioni
+function upBlock(i,t,path,tops,directions,randoms,counter,side) {
+  //fai parte del bordo superiore
+  if (searchInArrayOfArrays(tops,[t + 1,i + 1])) {
+    //sei un estremo del percorso
+    if (whereInArrayOfArrays(path,[t + 1,i + 1]) === 0 || whereInArrayOfArrays(path,[t + 1,i + 1]) === (path.length - 1)) {
+      return 0;
+    }
+    //non sei un estremo del percorso
+    else {
+      return 1;
+    }
+  }
+  //non fai parte del bordo superiore
+  else {
+    //fai parte del percorso
+    if (searchInArrayOfArrays(path,[t + 1,i + 1])) {
+      //la direzione di marcia `e top o la direzione del precedente `e down
+      if (directions[whereInArrayOfArrays(path,[t + 1,i + 1])] === 'up' || directions[whereInArrayOfArrays(path,[t + 1,i + 1]) - 1] === 'down') {
+        return 0;
+      }
+      //altro
+      else {
+        return randoms[counter];
+      }
+    }
+    //non fai parte del percorso
+    else {
+      //la parte sopra ha gi`a il bordo
+      if (randoms[counter - side] === 1) {
+        return 0;
+      }
+      //altro
+      else {
+        return randoms[counter];
+      }
+    }
+  }
+}
+
+function rightBlock(i,t,path,rights,directions,randoms,counter,side) {
+  if (searchInArrayOfArrays(rights,[t + 1,i + 1])) {
+    if (whereInArrayOfArrays(path,[t + 1,i + 1]) === 0 || whereInArrayOfArrays(path,[t + 1,i + 1]) === (path.length - 1)) {
+      return 0;
+    }
+    else {
+      return 1;
+    }
+  }
+  else {
+    if (searchInArrayOfArrays(path,[t + 1,i + 1])) {
+      if (directions[whereInArrayOfArrays(path,[t + 1,i + 1])] === 'right' || directions[whereInArrayOfArrays(path,[t + 1,i + 1]) - 1] === 'left') {
+        return 0;
+      }
+      else {
+        return randoms[counter];
+      }
+    }
+    else {
+      if (randoms[counter + 1] === 1) {
+        return 0;
+      }
+      else {
+        return randoms[counter];
+      }
+    }
+  }
+}
+
+function downBlock(i,t,path,downs,directions,randoms,counter,side) {
+  if (searchInArrayOfArrays(downs,[t + 1,i + 1])) {
+    if (whereInArrayOfArrays(path,[t + 1,i + 1]) === 0 || whereInArrayOfArrays(path,[t + 1,i + 1]) === (path.length - 1)) {
+      return 0;
+    }
+    else {
+      return 1;
+    }
+  }
+  else {
+    if (searchInArrayOfArrays(path,[t + 1,i + 1])) {
+      if (directions[whereInArrayOfArrays(path,[t + 1,i + 1])] === 'down' || directions[whereInArrayOfArrays(path,[t + 1,i + 1]) - 1] === 'up') {
+        return 0;
+      }
+      else {
+        return randoms[counter];
+      }
+    }
+    else {
+      if (randoms[counter + side] === 1) {
+        return 0;
+      }
+      else {
+        return randoms[counter];
+      }
+    }
+  }
+}
+
+function leftBlock(i,t,path,lefts,directions,randoms,counter,side) {
+  if (searchInArrayOfArrays(lefts,[t + 1,i + 1])) {
+    if (whereInArrayOfArrays(path,[t + 1,i + 1]) === 0 || whereInArrayOfArrays(path,[t + 1,i + 1]) === (path.length - 1)) {
+      return 0;
+    }
+    else {
+      return 1;
+    }
+  }
+  else {
+    if (searchInArrayOfArrays(path,[t + 1,i + 1])) {
+      if (directions[whereInArrayOfArrays(path,[t + 1,i + 1])] === 'left' || directions[whereInArrayOfArrays(path,[t + 1,i + 1]) - 1] === 'right') {
+        return 0;
+      }
+      else {
+        return randoms[counter];
+      }
+    }
+    else {
+      if (randoms[counter - 1] === 1) {
+        return 0;
+      }
+      else {
+        return randoms[counter];
+      }
+    }
+  }
+}
+
 class Square extends React.Component {
   render() {
     return (
@@ -51,7 +178,7 @@ class Game extends React.Component {
       directions: [],
       solution: false,
       randoms: [],
-      side: 10,
+      side: 20,
     };
   }
 
@@ -61,35 +188,46 @@ class Game extends React.Component {
     });
   }
 
-  go(border) {
+  go(border,tops,rights,bottoms,lefts) {
     var start = border[Math.floor(Math.random() * border.length)];
+    var first = 0;
 
-    var level1 = 0;
-    var level2 = 0;
+    //creare il path da capo
     var level3 = 0;
-
     do {
       level3++;
-      if (level1 > 100) {
-        alert('Riprova!');
-        break;
-      }
       var myPath = [];
       var myDirections= [];
       myPath.push(start);
+      //ciclo finch`e non riempio il path
+      var level2 = 0;
       do {
-        level2++;
-        if (level1 > 100) {
-          break;
-        }
+        level2++
         var last = myPath[myPath.length - 1];
         var current = [last[0],last[1]];
+        //ciclo finch`e la casella non `e non passata e finch`e non formo una figura chiusa
+        var level1 = 0;
         do {
           level1++;
-          if (level1 > 100) {
-            break;
+          var direction;
+          if (first === 0) {
+            if (searchInArrayOfArrays(tops,start)) {
+              direction = 'down';
+            }
+            else if (searchInArrayOfArrays(rights,start)) {
+              direction = 'left';
+            }
+            else if (searchInArrayOfArrays(bottoms,start)) {
+              direction = 'up';
+            }
+            else if (searchInArrayOfArrays(lefts,start)) {
+              direction = 'right';
+            }
           }
-          var direction = ['up','right','down','left'][getRandomInt(4) + 1];
+          else {
+            direction = ['up','right','down','left'][getRandomInt(4) + 1];
+          }
+          first++;
           var currentTry = [current[0],current[1]];
           switch (direction) {
             case 'up':
@@ -107,25 +245,27 @@ class Game extends React.Component {
             default:
               //
           }
-        } while(!(
-          currentTry[0] >= 1 &&
-          currentTry[0] <= 10 &&
-          currentTry[1] >= 1 &&
-          currentTry[1] <= 10
-        ) || searchInArrayOfArrays(myPath,currentTry));
+          if (level1 > 4) {
+            break;
+          }
+        } while(searchInArrayOfArrays(myPath,currentTry));
         myPath.push(currentTry);
         myDirections.push(direction);
-      } while (!searchInArrayOfArrays(border,currentTry) && myPath.length < 50);
+        if (level2 > 100) {
+          break;
+        }
+      } while (!searchInArrayOfArrays(border,currentTry));
+      console.log(myPath.length);
+      console.log(myPath);
+      if (level3 > 100) {
+        break;
+      }
     } while (myPath.length < 10)
-
-    console.log(myPath);
-    console.log(myDirections);
 
     var myRandoms = [];
     for (let s = 0; s < (this.state.side * this.state.side * 4); s++) {
       myRandoms.push(getRandomInt(2));
     }
-    console.log(myRandoms);
 
     this.setState({
       path: myPath,
@@ -152,7 +292,7 @@ class Game extends React.Component {
     }
     var border = tops.concat(rights).concat(bottoms).concat(lefts);    
 
-    if (this.state.randoms != []) {
+    if (this.state.randoms !== []) {
       //base
       var structure = [];
       var counter = 0;
@@ -163,10 +303,10 @@ class Game extends React.Component {
             key={'square-' + (t + 1) + '-' + (i + 1)}
             passed={(searchInArrayOfArrays(this.state.path,[t + 1,i + 1]) && this.state.solution)? true : false}
             show={this.state.solution? true : false}
-            up={(searchInArrayOfArrays(this.state.path,[t + 1,i + 1]) && this.state.directions[whereInArrayOfArrays(this.state.path,[t + 1,i + 1])] === 'up') || this.state.directions[whereInArrayOfArrays(this.state.path,[t + 1,i + 1]) - 1] === 'down'? 0 : this.state.randoms[counter]}
-            right={(searchInArrayOfArrays(this.state.path,[t + 1,i + 1]) && this.state.directions[whereInArrayOfArrays(this.state.path,[t + 1,i + 1])] === 'right') || this.state.directions[whereInArrayOfArrays(this.state.path,[t + 1,i + 1]) - 1] === 'left'? 0 : this.state.randoms[counter + 1]}
-            down={(searchInArrayOfArrays(this.state.path,[t + 1,i + 1]) && this.state.directions[whereInArrayOfArrays(this.state.path,[t + 1,i + 1])] === 'down') || this.state.directions[whereInArrayOfArrays(this.state.path,[t + 1,i + 1]) - 1] === 'up'? 0 : this.state.randoms[counter + 2]}
-            left={(searchInArrayOfArrays(this.state.path,[t + 1,i + 1]) && this.state.directions[whereInArrayOfArrays(this.state.path,[t + 1,i + 1])] === 'left') || this.state.directions[whereInArrayOfArrays(this.state.path,[t + 1,i + 1]) - 1] === 'right'? 0 : this.state.randoms[counter + 3]}
+            up={upBlock(i,t,this.state.path,tops,this.state.directions,this.state.randoms,counter,this.state.side)}
+            right={rightBlock(i,t,this.state.path,rights,this.state.directions,this.state.randoms,counter,this.state.side)}
+            down={downBlock(i,t,this.state.path,bottoms,this.state.directions,this.state.randoms,counter,this.state.side)}
+            left={leftBlock(i,t,this.state.path,lefts,this.state.directions,this.state.randoms,counter,this.state.side)}
             direction={(searchInArrayOfArrays(this.state.path,[t + 1,i + 1]))? this.state.directions[whereInArrayOfArrays(this.state.path,[t + 1,i + 1])] : 0}
             />);
             counter = counter + 4;
@@ -177,7 +317,7 @@ class Game extends React.Component {
 
     return (
       <div>
-        <button className="new" onClick={() => this.go(border)}>Nuovo</button>
+        <button className="new" onClick={() => this.go(border,tops,rights,bottoms,lefts)}>Nuovo</button>
         <button className="solution" onClick={() => this.show()}>Soluzione</button>
         <div className="structure">{structure}</div>
       </div>
