7fa0d020fcbd81ae3b6c217cff2c8fc374eb3994
laura.caldaronello <caldaronello.l@gmail.com>
2021-07-28 14:44:19 +0200

    cambiare algoritmo

--- src/index.js
+++ src/index.js
@@ -24,6 +24,30 @@ function whereInArrayOfArrays(bigArray,smallArray){
   return -1;
 }
 
+//ci sono duplicati?
+function areThereDuplicates(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let t = i + 1; t < array.length; t++) {
+      if (array[i].toString() === array[t].toString()) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+//elimino le figure chiuse
+function deleteFirstCircle(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let t = i + 1; t < array.length; t++) {
+      if (array[i].toString() === array[t].toString()) {
+        array.splice(i + 1,t);
+        return array;
+      }
+    }
+  }
+  return array;
+}
 
 class Horizontal extends React.Component {
   render() {
@@ -36,10 +60,18 @@ class Horizontal extends React.Component {
 class Vertical extends React.Component {
   render() {
     return (
+      <div className="vertical"></div>
+    );
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    return (
       <div 
-        className="vertical"
+        className="square"
         style={{
-          marginRight: this.props.marginRight + 'px',
+          backgroundColor: (this.props.passed && this.props.solution)? 'yellow' : 'lightgoldenrodyellow'
         }}
       ></div>
     );
@@ -57,6 +89,21 @@ class Game extends React.Component {
       side: 20,
     };
   }
+  
+  //rendering
+  horizontalRender(r,i) {
+    return <Horizontal key={'horizontal-' + r + '-' + i}/>
+  };
+  verticalRender(r,i) {
+    return <Vertical key={'vertical-' + r + '-' + i}/>
+  };
+  squareRender(r,i) {
+    return <Square 
+      key={'square-' + r + '-' + i}
+      passed={(searchInArrayOfArrays(this.state.path,[r,i])? true : false)}
+      solution={this.state.solution? true : false}
+    />
+  };
 
   show() {
     this.setState({
@@ -66,6 +113,93 @@ class Game extends React.Component {
 
   go() {
 
+    //bordi
+    var tops = [];
+    var rights = [];
+    var bottoms = [];
+    var lefts = [];
+    for (let i = 1; i < this.state.side - 1; i++) {
+      tops.push([0,i]);
+      rights.push([i,this.state.side - 1]);
+      bottoms.push([this.state.side - 1,i]);
+      lefts.push([i,0]);
+    }
+    
+    //start
+    var border = tops.concat(rights).concat(bottoms).concat(lefts);
+    var start = border[getRandomInt(border.length)];
+    var myPath = [];
+    myPath.push(start);
+
+    var direction;
+    if (searchInArrayOfArrays(tops,start)) {
+      direction = 'bottom';
+      myPath.push([start[0] + 1,start[1]]);
+    }
+    else if (searchInArrayOfArrays(rights,start)) {
+      direction = 'left';
+      myPath.push([start[0],start[1] - 1]);
+    }
+    else if (searchInArrayOfArrays(bottoms,start)) {
+      direction = 'top';
+      myPath.push([start[0] - 1,start[1]]);
+    }
+    else if (searchInArrayOfArrays(lefts,start)) {
+      direction = 'right';
+      myPath.push([start[0],start[1] + 1]);
+    }
+    var myDirections = [];
+    myDirections.push(direction);
+
+    //prosecuzione
+    var current;
+    var currentTry;
+    do {
+      do {
+        current = myPath[myPath.length - 1];
+        do {
+          currentTry = [current[0],current[1]];
+          direction = ['top','right','bottom','left'][getRandomInt(4)];
+          if (direction === 'top') {
+            currentTry[0]--;
+          }
+          else if (direction === 'right') {
+            currentTry[1]++;
+          }
+          else if (direction === 'bottom') {
+            currentTry[0]++;
+          }
+          else if (direction === 'left') {
+            currentTry[1]--;
+          }
+        } while (
+            myDirections[myDirections.length - 1] === direction + 2 || myDirections[myDirections.length - 1] === direction - 2 //non posso tornare indietro alla casella precedente
+          )
+        myDirections.push(direction);
+        myPath.push(currentTry);
+      } while (!searchInArrayOfArrays(border,myPath[myPath.length - 1])) //finch`e non tocco il bordo
+    } while (myPath.length < 10) //non troppo corto
+
+    for (let i = 0; i < myPath.length; i++) {
+      console.log(myPath[i]);
+    }
+
+    console.log('____________________________________________');
+
+    //while (areThereDuplicates(myPath)) {
+    //  myPath = deleteFirstCircle(myPath);
+    //}
+
+    for (let i = 0; i < myPath.length; i++) {
+      console.log(myPath[i]);
+    }
+
+    this.setState({
+      path: myPath,
+      directions: myDirections,
+      solution: false,
+    });
+
   }
 
   render() {
@@ -74,30 +208,30 @@ class Game extends React.Component {
     for (var r = 0; r < this.state.side; r++) {
       var rowHorizontal = [];
       for (let i = 0; i < this.state.side; i++) {
-        rowHorizontal.push(<Horizontal key={'horizontal-' + r + '-' + i}/>);
+        rowHorizontal.push(this.horizontalRender(r,i));
       }
       var rowVertical = [];
-      for (let i = 0; i < this.state.side; i++) {
-        rowVertical.push(<Vertical 
-          key={'vertical-' + r + '-' + i}
-          marginRight={this.state.side * 2}
-        />);
+      for (var i = 0; i < this.state.side; i++) {
+        rowVertical.push(this.verticalRender(r,i));
+        rowVertical.push(this.squareRender(r,i));
       }
-      structure.push(<div className="noMargin">{rowHorizontal}</div>,<div className="noMargin">{rowVertical}</div>);
+      rowVertical.push(this.verticalRender(r,i));
+      structure.push(<div className="noMargin" key={'row-horizontal-' + r}>{rowHorizontal}</div>,<div className="noMargin" key={'row-vertical-' + r}>{rowVertical}</div>);
     }
-    var rowHorizontal = [];
+    rowHorizontal = [];
     for (let i = 0; i < this.state.side; i++) {
-      rowHorizontal.push(<Horizontal key={'horizontal-' + r + '-' + i}/>);
+      rowHorizontal.push(this.horizontalRender(r,i));
     }
-    structure.push(<div className="noMargin">{rowHorizontal}</div>);
+    structure.push(<div className="noMargin" key={'row-horizontal-' + r}>{rowHorizontal}</div>);
 
     return (
       <div>
         <button className="new" onClick={() => this.go()}>Nuovo</button>
         <button className="solution" onClick={() => this.show()}>Soluzione</button>
-        <div>{structure}</div>
+        <div className="structure">{structure}</div>
       </div>
     );
+
   }
 }
 
